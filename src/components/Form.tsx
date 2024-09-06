7cdcad6747432022d4aa4d637b315ce9e78d9e44
AnubioG <rubioporrasangela@gmail.com>
2024-09-06 15:15:54 -0500

    Estilos para la validación del formulario

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -17,6 +17,9 @@ export function Form() {
     setRepaymentOnly(data);
     calculateMonthlyAmount(data);
     calculateTotalInterest(data);
+  };
+
+  const handleClick = () => {
     reset();
   };
 
@@ -26,7 +29,9 @@ export function Form() {
         <h2 className="text-2xl font-bold text-cyan-950">
           Amortiza tu Crédito
         </h2>
-        <button className=" underline">Reiniciar Formulario</button>
+        <button className="underline" onClick={handleClick}>
+          Reiniciar Formulario
+        </button>
       </div>
       <form
         className="bg-white text-sm text-cyan-950"
@@ -34,19 +39,35 @@ export function Form() {
         onSubmit={handleSubmit(calculateMortgage)}
       >
         <div className="mb-6">
-          <label htmlFor="amount" className="text-md  ">
+          <label htmlFor="amount" className="text-md ">
             Valor del Crédito
           </label>
-          <input
-            id="amount"
-            className="w-full p-3 rounded-md border border-cyan-950 mt-2 "
-            type="number"
-            placeholder="Monto total de la hipoteca"
-            {...register("amount", {
-              required: "El valor es un campo requerido",
-            })}
-          />
-          {errors.amount && <p>{errors.amount.message}</p>}
+          <div
+            className={`flex items-center rounded-md border  border-cyan-950 mt-2 ${
+              errors.amount ? " border-red-600" : "  border-cyan-950"
+            } `}
+          >
+            <span
+              className={`px-4 py-3 text-center rounded-s-md font-bold ${
+                errors.amount
+                  ? "bg-red-600 text-white"
+                  : " bg-[#E5F2FE] text-[#0E2431]"
+              }`}
+            >
+              $
+            </span>
+            <input
+              id="amount"
+              type="number"
+              className="w-full ps-4 rounded-e-md py-3 focus:outline-none font-bold"
+              {...register("amount", {
+                required: "El valor es un campo requerido",
+              })}
+            />
+          </div>
+          {errors.amount && (
+            <p className="text-red-600 mt-2">{errors.amount.message}</p>
+          )}
         </div>
 
         <div className="flex flex-col md:flex-row gap-3 w-full justify-between">
@@ -54,33 +75,66 @@ export function Form() {
             <label htmlFor="term" className="text-md  ">
               Plazo
             </label>
-            <input
-              id="term"
-              className="w-full p-3 rounded-md border border-cyan-950 mt-2"
-              type="number"
-              placeholder="Plazo en # de meses"
-              {...register("term", {
-                required: "El plazo es un campo requerido",
-              })}
-            />
-            {errors.term && <p>{errors.term.message}</p>}
+            <div
+              className={`flex items-center rounded-md border  border-cyan-950 mt-2 ${
+                errors.amount ? " border-red-600" : "  border-cyan-950"
+              } `}
+            >
+              <input
+                id="term"
+                className="w-full ps-4 rounded-s-md py-3 focus:outline-none font-bold"
+                type="number"
+                {...register("term", {
+                  required: "El plazo es un campo requerido",
+                })}
+              />
+              <span
+                className={`px-4 py-3 text-center rounded-e-md font-bold ${
+                  errors.amount
+                    ? "bg-red-600 text-white"
+                    : " bg-[#E5F2FE] text-[#0E2431]"
+                }`}
+              >
+                meses
+              </span>
+            </div>
+            {errors.term && (
+              <p className="text-red-600 mt-2">{errors.term.message}</p>
+            )}
           </div>
 
           <div className="mb-6">
             <label htmlFor="interest" className="text-md  ">
-              Porcentaje de interes
+              Interés
             </label>
 
-            <input
-              id="interest"
-              className="w-full p-3 rounded-md border border-cyan-950 mt-2"
-              type="number"
-              placeholder="% de intereses"
-              {...register("interest", {
-                required: "El interés es un campo requerido",
-              })}
-            />
-            {errors.interest && <p>{errors.interest.message}</p>}
+            <div
+              className={`flex items-center rounded-md border  border-cyan-950 mt-2 ${
+                errors.amount ? " border-red-600" : "  border-cyan-950"
+              } `}
+            >
+              <input
+                id="interest"
+                className="w-full ps-4 rounded-s-md py-3 focus:outline-none font-bold"
+                type="number"
+                {...register("interest", {
+                  required: "El interés es un campo requerido",
+                })}
+              />
+              <span
+                className={`px-4 py-3 text-center rounded-e-md font-bold ${
+                  errors.amount
+                    ? "bg-red-600 text-white"
+                    : " bg-[#E5F2FE] text-[#0E2431]"
+                }`}
+              >
+                %
+              </span>
+            </div>
+
+            {errors.interest && (
+              <p className="text-red-600 mt-2">{errors.interest.message}</p>
+            )}
           </div>
         </div>
 
@@ -89,7 +143,7 @@ export function Form() {
           <div className="flex flex-col gap-1">
             <label
               htmlFor="amortizacion"
-              className="flex items-center w-full p-3 rounded-md border border-cyan-950 mt-2"
+              className="flex items-center w-full p-3 rounded-md border border-cyan-950 mt-2 hover:border-[#D8DB2F] hover:bg-[#FAFAE0]"
             >
               <input
                 id="amortizacion"
@@ -99,24 +153,27 @@ export function Form() {
                   required: "Debe seleccionar tipo de cálculo",
                 })}
               />
-              <span className="ml-2">Valor mensual e intereses</span>
+              <span className="ml-2 font-bold">Valor mensual e intereses</span>
             </label>
             <label
               htmlFor="solo-intereses"
-              className="flex items-center w-full p-3 rounded-md border border-cyan-950 mt-2"
+              className="flex items-center w-full p-3 rounded-md border border-cyan-950 mt-2 hover:border-[#D8DB2F] hover:bg-[#FAFAE0]"
             >
               <input
                 id="solo-intereses"
                 type="radio"
                 value="2"
+                className=" active:text-cyan-700"
                 {...register("repaymentOnly", {
                   required: "Debes seleccionar tipo de cálculo",
                 })}
               />
-              <span className="ml-2">Sólo intereses</span>
+              <span className="ml-2 font-bold">Sólo intereses</span>
             </label>
           </div>
-          {errors.repaymentOnly && <p>{errors.repaymentOnly.message}</p>}
+          {errors.repaymentOnly && (
+            <p className="text-red-600 mt-2">{errors.repaymentOnly.message}</p>
+          )}
         </div>
 
         <input
